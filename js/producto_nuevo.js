cb30b20f1ce4182156cf4be74683972a724cc08c
dario923 <sanajani410@gmail.com>
2023-07-11 10:33:38 -0300

    subiendo los primeros archivos

---
+++ js/producto_nuevo.js
@@ -0,0 +1,32 @@
+function guardar() {
+    let n = document.getElementById("nombre").value
+    let p = parseFloat(document.getElementById("precio").value)
+    let s = parseInt(document.getElementById("stock").value)
+    let i = document.getElementById("imagen").value
+    
+    let producto = {
+        nombre: n,
+        precio: p,
+        stock: s,
+        imagen: i
+    }
+    let url = "https://sdario.pythonanywhere.com/productos"
+    var options = {
+        body: JSON.stringify(producto),
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+    }
+    fetch(url, options)
+        .then(function () {
+            console.log("creado")
+            alert("Grabado")
+            window.location.href = "./productos.html";  
+            // Handle response we get from the API
+        })
+        .catch(err => {
+            //this.errored = true
+            alert("Error al grabar" )
+            console.error(err);
+        })
+}
+
