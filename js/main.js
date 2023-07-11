bff3556cd1596b8703b1873657f286fa1bdd7c6f
dario923 <sanajani410@gmail.com>
2023-07-11 19:21:53 -0300

    agregue codigo

--- js/main.js
+++ js/main.js
@@ -1,37 +1,15 @@
 document.getElementById("header").innerHTML=`<nav class="navbar navbar-expand-sm navbar-light bg-light">
-<div class="container">
-  <!--<a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-      aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="collapsibleNavId">
-      <ul class="navbar-nav me-auto mt-2 mt-lg-0">
-          <li class="nav-item">
-              <a class="nav-link active" href="#" aria-current="page">Home <span class="visually-hidden">(current)</span></a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">CRUD</a>
-              <div class="dropdown-menu" aria-labelledby="dropdownId">
-                  <a class="dropdown-item" href="productos.html">Productos</a>
-                  <a class="dropdown-item" href="#">Action 2</a>
-              </div>
-          </li>
-      </ul>-->
       <nav class="navbar navbar-expand-lg" data-bs-theme="dark">
             <div class="container-fluid">
                 <img  class="navbar-brand logo_header" id="logo" src="./img/favicon.ico" alt="Logo">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <a class="navbar-brand" href="index.html">Digital New's</a>
+                <a class="navbar-brand" href="index.html">New's Digital</a>
                 <div class="collapse navbar-collapse nav justify-content-end" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a href="index.html" class="nav-link active">| Inicio</a>
+                            <a href="productos.html" class="nav-link active">| Inicio</a>
                         </li>
                         <li class="nav-item">
                             <a href="productos.html" class="nav-link active">| Lista De Productos</a>
